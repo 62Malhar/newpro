be2ddcaf2478cdc8e71e67b08ac210bfae1acf79
malharpandya <mlhrpandya@gmail.com>
2021-02-04 11:37:14 +0530

    12screens

--- App.js
+++ App.js
@@ -1,107 +1,24 @@
-import React, { useState,useEffect } from 'react';
-import {
-  Text,
-  View,
-  Button,
-  Image,
-  Dimensions,
-  FlatList,
-} from 'react-native';
+import React,{useState,useEffect} from 'react';
+import {View,Text,Image,TextInput,Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Splash from './s2/Splash';
+import Login from './s2/Login';
+import MyProfile from './s2/MyProfile';
+const Stack=createStackNavigator();
+const Tab=createBottomTabNavigator();
+const Drawer=createDrawerNavigator();
 
-let queen = [
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNXwTb5z0NlcwU8kaSIeV1DOmZtPgadUZiBA&usqp=CAU',
-    name: 'James',
-    txt: '3 days ago',
-  },
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQaIBGjQXotQNadmFrvhZK60-VSBHwAIcdCQ&usqp=CAU',
-    name: 'Steve',
-    txt: '5 days ago',
-  },
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO06pOmBndDO0isERXC_9xx43Y3YB7rDDnBg&usqp=CAU',
-    name: 'Bob',
-    txt: '4 days ago',
-  },
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxgnA7-hYNCoWrwQ06oJNZIeu42d-hndUqMg&usqp=CAU',
-    name: 'Ricky',
-    txt: '2 days ago',
-  },
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKqGgDU7ueNdtCwqFcr4DgsNkfnKBHt-4-g&usqp=CAU',
-    name: 'Carter',
-    txt: '7 days ago',
-  },
-  {
-    img1:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSle7CIpf_CtDpxbNkuKQHIJxRuXWlNMTv1lw&usqp=CAU',
-    name: 'Shane',
-    txt: '6 days ago',
-  },
-];
-
-export default function App() {
-  
-  const[numColumns,setNumcolumns]=useState(1);
-  const [count, setCount] = useState(2);
-  const[grid,setGrid]=useState(false);
-  const[title,setTitle]=useState(' SHOW GRID');
-  const screenWidth = Dimensions.get("window").width;
-  const tileSize = screenWidth / numColumns;
-
-function check(){
-  setCount(count+1);
-if(count%2==0)
-{
-setGrid(false);
-setTitle('SHOW GRID');
-setNumcolumns(1);
-} else{
-  setGrid(true);
-  setTitle('SHOW LIST');
-  setNumcolumns(2);
-} 
-
-}
-
-
-  return (
-    <View style={{backgroundColor:'black'}}>
-      <Button title={title} onPress={() => check()} />
-      <FlatList
-        horizontal={false}
-  
- key={(grid) ? 1 : 0}
- numColumns={grid ? 2 : 1}
-
-        data={queen}
-        renderItem={({item,index})=>
-             <View style={{marginTop:10}}>
-            <Image
-              source={{ uri: item.img1 }}
-              style={{
-                width: tileSize,
-                height: tileSize,
-                marginTop: 5,
-                borderWidth: 1,
-                marginLeft:5,
-              }}
-            />
-                        <Text style={{ fontSize: 15,color:'white' }}>{item.name}</Text>
-
-            <Text style={{ fontSize: 15,color:'white' }}>{item.txt}</Text>
-          </View>
-        }
-        
-      />
-    </View>
+export default function App(){
+  return(
+<NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SOCIAL NETWORK" component={MyProfile}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-  
---
+++ s2/Mymemories.js
@@ -0,0 +1,70 @@
+import { StackActions } from '@react-navigation/native';
+import React,{useState} from 'react';
+import { View, Text, Button, Image, ScrollView, FlatList,TouchableHighlight } from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Foundation from 'react-native-vector-icons/Foundation';
+
+let Memo = [
+  {
+    img:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIDEElfXr-huCic72Db0beO7owu9HBeIF5yA&usqp=CAU',
+    txt1: 'Lorem Ipsum Dorol',
+    txt2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  },
+  {
+    img:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXY5O2YpqZV5EgQnuqbJ-pdCZlZxhzlHaSwA&usqp=CAU',
+    txt1: 'Lorem Ipsum Dorol',
+     txt2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  },
+  {
+    img:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWSODeFV-mS4nGC6IkNPofeT-W89bEKXSG-A&usqp=CAU',
+    txt1: 'Lorem Ipsum Dorol',
+     txt2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  },
+  {
+    img:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT3toWW0_3Q6yXkaja2yuPA_1CUPUFr3gsvA&usqp=CAU',
+    txt1: 'Lorem Ipsum Dorol',
+     txt2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  },
+];
+
+function Mymemories1() {
+  const[count,setCount]=useState(0);
+  return (
+    <ScrollView>
+      <FlatList
+        data={Memo}
+        renderItem={({ item }) => (
+          <View>
+            <View style={{ flexDirection: 'row' ,marginTop:20,marginLeft:5,borderBottomWidth:3}}>
+              <Image
+                source={{ uri: item.img }}
+                style={{ width: 120, height: 120 }}
+              />
+              <View style={{flexDirection:'column'}}>
+              <Text style={{fontSize:20,fontWeight:'bold'}}>{item.txt1}</Text>
+ <Text numberOfLines={4}>{item.txt2}</Text>
+ <View style={{flexDirection:'row',marginTop:40}}> 
+ <TouchableHighlight onPress={()=>setCount(count+1)}>
+<AntDesign name='hearto' color='black' size={17} /></TouchableHighlight><Text>{count}</Text>
+<Foundation name='comment' color='blue' size={18} style={{marginLeft:80}}/><Text>{count}</Text>
+  </View>
+              </View>
+            </View>
+          </View>
+        )}
+      />
+    </ScrollView>
+  );
+}
+const Stack=createStackNavigator();
+export default function Mymemories(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='My Memories' component={Mymemories1}/>
+    </Stack.Navigator>
+  );
+}
